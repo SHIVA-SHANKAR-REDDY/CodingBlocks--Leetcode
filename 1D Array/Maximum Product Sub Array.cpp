c8876687b2f682c1b1df8c69d5c85aa0c81bedc6
SHIVA SHANKAR REDDY <103198871+SHIVA-SHANKAR-REDDY@users.noreply.github.com>
2023-08-15 19:26:12 +0530

    Update Maximum Product Sub Array.cpp

--- 1D Array/Maximum Product Sub Array.cpp
+++ 1D Array/Maximum Product Sub Array.cpp
@@ -16,3 +16,20 @@ public:
         return ans;
     }
 };
+
+//Dynamic Programming approach
+int maxProduct(vector<int>& nums) {
+        int cur_min=1,cur_max=1;
+        int ans=max(nums);
+        for(int i=0;i<nums.size();i++)
+        {
+            if(cur_min==0) cur_min=1;
+            if(cur_max==0) cur_max=1;
+
+            int temp=cur_max*n;
+            cur_max=max(n*cur_max,n*cur_min,n);
+            cur_min=min(tmp,n*cur_min,n);
+            ans=max(ans,max(cur_min,cur_max));
+        }
+        return ans;
+    }
